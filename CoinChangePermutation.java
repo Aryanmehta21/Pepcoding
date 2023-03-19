867e2b102c38180afddd41e07b20516bf6de1d24
Aryanmehta21 <aryanmehta310@gmail.com>
2023-03-19 15:26:07 +0530

    added coin change permutation

---
+++ CoinChangePermutation.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class CoinChangePermutation {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i =0;i<arr.length;i++){
+            arr[i] = sc.nextInt();
+        }
+        int target = sc.nextInt();
+
+        int[] dp = new int[target + 1];
+        
+    }
+}
