1404f26882945b61da10a9752442ae654e9b0a12
Aryanmehta21 <aryanmehta310@gmail.com>
2023-04-15 00:26:59 +0530

    backdate

--- subarray_problem.java
+++ subarray_problem.java
@@ -1,27 +1,22 @@
 // import java.io.*;
 import java.util.*;
-
 public class subarray_problem{
-
-public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception {
     // write your code here
-    Scanner sc = new Scanner(System.in);
-    int n = sc.nextInt();
-    int arr[] = new int[n];
-    for(int i =0;i<n;i++){
-        arr[i] = sc.nextInt();
-    }
-    
-    
-    for(int i =0;i<arr.length;i++){
-        for(int j =i;j<arr.length;j++){
-            for(int k = i ;k<=j;k++){
-                System.out.print(arr[k] + " ");
-            }
-        System.out.println("");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for(int i =0;i<n;i++){
+            arr[i] = sc.nextInt();
         }
+        for(int i =0;i<arr.length;i++){
+            for(int j =i;j<arr.length;j++){
+                for(int k = i ;k<=j;k++){
+                    System.out.print(arr[k] + " ");
+                }
+                System.out.println("");
+            }
+        }
+        sc.close();
     }
-    sc.close();
- }
-
 }
