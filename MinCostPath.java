bfd6f80ef7965f54085bdeedad9c7b3c562fe14a
Aryanmehta21 <aryanmehta310@gmail.com>
2023-05-16 00:37:13 +0530

    Update MinCostPath.java

--- MinCostPath.java
+++ MinCostPath.java
@@ -13,7 +13,7 @@ public class MinCostPath {
 
         int[][] dp = new int[n][m];
         for(int i =dp.length-1;i>=0;i--){
-            for(int j = dp[0].length-1;j>=0;j++){
+            for(int j = dp[0].length-1;j>=0;j--){
                 if(i == dp.length-1 && j==dp[0].length-1){
                     dp[i][j] = arr[i][j];
                 }else if(i == dp.length-1){
