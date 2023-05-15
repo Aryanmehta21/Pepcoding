a9c59c58dafc40eea0b6065fe2041bf6593c864a
Aryanmehta21 <aryanmehta310@gmail.com>
2023-05-16 00:27:46 +0530

    Update MinCostPath.java

--- MinCostPath.java
+++ MinCostPath.java
@@ -4,6 +4,18 @@ public class MinCostPath {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int m = sc.nextInt();
-        
+        int[][] arr = new int[n][m];
+        for(int i =0;i<n;i++){
+            for(int j =0;j<m;j++){
+                arr[i][j] = sc.nextInt();
+            }
+        }
+
+        int[][] dp = new int[n][m];
+        for(int i =n-1;i>=0;i--){
+            for(int j = m-1;j>=0;j++){
+                
+            }
+        }
     }
 }
