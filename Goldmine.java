6ef74c9be2f6a8eeefad12993318fe3016fb3d06
Aryanmehta21 <aryanmehta310@gmail.com>
2023-05-18 21:57:10 +0530

    Update Goldmine.java

--- Goldmine.java
+++ Goldmine.java
@@ -13,6 +13,28 @@ public class Goldmine{
 
         int[][] dp = new int[row][col];
 
-        
+        for(int j = mine[0].length-1;j>=0;j--){
+            for(int i = mine.length-1;i>=0;i--){
+                if(j == mine[0].length-1){
+                    dp[i][j] = mine[i][j];
+                }else if(i == 0){
+                    dp[i][j] = mine[i][j] + Math.max(dp[i][j+1], dp[i+1][j+1]);
+                }else if(i == mine.length-1){
+                    dp[i][j] = mine[i][j] + Math.max(dp[i][j+1], dp[i-1][j+1]);
+                }else{
+                    dp[i][j] = mine[i][j] + Math.max(dp[i][j+1], Math.max(dp[i+1][j+1], dp[i-1][j-1]));
+                }
+            }
+        }
+
+
+        int max = dp[0][0];
+        for(int i =0;i<dp.length;i++){
+            if(dp[i][0]>max){
+                max = dp[i][0];
+            }
+        }
+
+        System.out.println(max);
     }
 }
