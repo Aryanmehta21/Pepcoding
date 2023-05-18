cffa3a9a76c663ebb882b275b50d2fdc11aedbf4
Aryanmehta21 <aryanmehta310@gmail.com>
2023-05-18 17:41:41 +0530

    Update EqualSum.java

--- EqualSum.java
+++ EqualSum.java
@@ -14,7 +14,14 @@
 import java.util.*;
 public class EqualSum {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i =0;i<n;i++){
+            arr[i] = sc.nextInt();
+        }
+        System.out.println(equilibrium(arr, n));
+    
     }
 
     public static String equilibrium(int arr[], int n) {
