fc993e36396541601cfd33f05fcee90a16bc2975
Aryanmehta21 <aryanmehta310@gmail.com>
2023-06-16 00:53:40 +0530

    0-1 Knapsack

---
+++ ZeroOneKnapSack.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.*;
+
+public class ZeroOneKnapSack {
+
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] vals = new int[n];
+        int[] wts = new int[n];
+        
+        for(int i =0;i<n;i++){
+            vals[i] = sc.nextInt();
+        }
+        
+        for(int i =0;i<n;i++){
+            wts[i] = sc.nextInt();
+        }
+        
+        int capacity = sc.nextInt();
+        
+        
+        int[][] dp = new int[n+1][capacity+1];
+        
+        
+        for(int i =1;i<dp.length;i++){
+            for(int j=1;j<dp[0].length;j++){
+                if(j>=wts[i-1]){
+                    int rCap = j-wts[i-1];
+                    if(dp[i-1][rCap]+vals[i-1]>dp[i-1][j]){
+                        dp[i][j] = dp[i-1][rCap] + vals[i-1];
+                    }else{
+                        dp[i][j] = dp[i-1][j];
+                    }
+                }else{
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+        
+        System.out.println(dp[n][capacity]);
+    }
+}
