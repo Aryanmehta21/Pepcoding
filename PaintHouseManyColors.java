7f8bb8f91c0d7f7d3ba73b27a75d7f085e9b9595
Aryanmehta21 <aryanmehta310@gmail.com>
2023-06-18 22:34:06 +0530

    Update PaintHouseManyColors.java

--- PaintHouseManyColors.java
+++ PaintHouseManyColors.java
@@ -16,26 +16,26 @@ public class PaintHouseManyColors {
             dp[0][j] = arr[0][j];
         }
         // n^3 Solution.
-        for(int i=1;i<dp.length;i++){
-            for(int j =0;j<dp[0].length;j++){
-                int min = Integer.MAX_VALUE;
-                for(int k = 0;k<dp[0].length;k++){
-                    if(k!=j){
-                        if(dp[i-1][k] < min){
-                            min = dp[i-1][k];
-                        }
-                    }
-                }
-                dp[i][j] = arr[i][j] + min;
-            }
-        }
+        // for(int i=1;i<dp.length;i++){
+        //     for(int j =0;j<dp[0].length;j++){
+        //         int min = Integer.MAX_VALUE;
+        //         for(int k = 0;k<dp[0].length;k++){
+        //             if(k!=j){
+        //                 if(dp[i-1][k] < min){
+        //                     min = dp[i-1][k];
+        //                 }
+        //             }
+        //         }
+        //         dp[i][j] = arr[i][j] + min;
+        //     }
+        // }
 
-        int min = Integer.MAX_VALUE;
-        for(int k =0;k<dp[0].length;k++){
-            if(dp[n-1][k] < min){
-                min = dp[n-1][k];
-            }
-        }
-        System.out.println(min);
+        // int min = Integer.MAX_VALUE;
+        // for(int k =0;k<dp[0].length;k++){
+        //     if(dp[n-1][k] < min){
+        //         min = dp[n-1][k];
+        //     }
+        // }
+        // System.out.println(min);
     }
 }
