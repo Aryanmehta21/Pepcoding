0b1c85712c25fa3804fc0805b66cb9a5a6d394b0
Aryanmehta21 <aryanmehta310@gmail.com>
2023-06-18 19:00:13 +0530

    Paint House

---
+++ PaintHouse.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class PaintHouse {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[][] arr = new int[n][3];
+        for(int i =0;i<arr.length;i++){
+            arr[i][j]= sc.nextInt();
+        }
+    }
+
+    long[][] dp = new long[n][3];
+}
