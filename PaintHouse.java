3683e432d771b6907e0948a4a780f88fa84794c6
Aryanmehta21 <aryanmehta310@gmail.com>
2023-06-18 19:05:19 +0530

    Paint House

--- PaintHouse.java
+++ PaintHouse.java
@@ -6,7 +6,9 @@ public class PaintHouse {
         int n = sc.nextInt();
         int[][] arr = new int[n][3];
         for(int i =0;i<arr.length;i++){
-            arr[i][j]= sc.nextInt();
+            for(int j=0;j<arr[0].length;j++){
+                arr[i][j]= sc.nextInt();
+            }
         }
         long[][] dp = new long[n][3];
 
