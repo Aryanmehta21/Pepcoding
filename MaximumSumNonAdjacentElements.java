652fd22ff164cb00798af78f5bb0328387db3a92
Aryanmehta21 <aryanmehta310@gmail.com>
2023-06-18 18:27:00 +0530

    Maximum Sum Non Adjacent Elements

--- MaximumSumNonAdjacentElements.java
+++ MaximumSumNonAdjacentElements.java
@@ -7,6 +7,17 @@ public class MaximumSumNonAdjacentElements {
         for(int i =0;i<n;i++){
             arr[i] = sc.nextInt();
         }
-        
+        int inc = arr[0];
+        int exc = 0;
+
+        for(int i =1;i<arr.length;i++){
+            int ninc = exc + arr[i];
+            int nexc = Math.max(inc, exc);
+
+            inc = ninc;
+            exc = nexc;
+        }
+
+        System.out.println(Math.max(inc, exc));
     }
 }
