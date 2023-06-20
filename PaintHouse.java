c7259437cc0b5cf1cd211ba526b5728d44117d76
Aryanmehta21 <aryanmehta310@gmail.com>
2023-06-21 00:22:29 +0530

    .

--- PaintHouse.java
+++ PaintHouse.java
@@ -13,7 +13,6 @@ public class PaintHouse {
         dp[0][0] = arr[0][0];
         dp[0][1] = arr[0][1];
         dp[0][2] = arr[0][2];
-
         for(int i =1;i<arr.length;i++){
             dp[i][0] = arr[i][0] + Math.min(dp[i-1][1], dp[i-1][2]);
             dp[i][1] = arr[i][1] + Math.min(dp[i-1][0], dp[i-1][2]);
