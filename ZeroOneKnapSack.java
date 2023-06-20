6c14508134b2f7b7d0561a1ed5fe72ead2630a26
Aryanmehta21 <aryanmehta310@gmail.com>
2023-06-20 20:36:47 +0530

    .

--- ZeroOneKnapSack.java
+++ ZeroOneKnapSack.java
@@ -1,43 +1,44 @@
-import java.util.*;
+// import java.util.*;
 
-public class ZeroOneKnapSack {
+// public class ZeroOneKnapSack {
 
-    public static void main(String[] args) throws Exception {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int[] vals = new int[n];
-        int[] wts = new int[n];
+//     public static void main(String[] args) throws Exception {
+//         Scanner sc = new Scanner(System.in);
+//         int n = sc.nextInt();
+//         int[] vals = new int[n];
+//         int[] wts = new int[n];
         
-        for(int i =0;i<n;i++){
-            vals[i] = sc.nextInt();
-        }
+//         for(int i =0;i<n;i++){
+//             vals[i] = sc.nextInt();
+//         }
         
-        for(int i =0;i<n;i++){
-            wts[i] = sc.nextInt();
-        }
+//         for(int i =0;i<n;i++){
+//             wts[i] = sc.nextInt();
+//         }
         
-        int capacity = sc.nextInt();
-        
-        
-        int[][] dp = new int[n+1][capacity+1];
+//         int capacity = sc.nextInt();
         
         
-        for(int i =1;i<dp.length;i++){
-            for(int j=1;j<dp[0].length;j++){
-                if(j>=wts[i-1]){
-                    int rCap = j-wts[i-1];
-                    if(dp[i-1][rCap]+vals[i-1]>dp[i-1][j]){
-                        dp[i][j] = dp[i-1][rCap] + vals[i-1];
-                    }else{
-                        dp[i][j] = dp[i-1][j];
-                    }
-                }else{
-                    dp[i][j] = dp[i-1][j];
-                }
-            }
-        }
+//         int[][] dp = new int[n+1][capacity+1];
         
-        System.out.println(dp[n][capacity]);
-        sc.close();
-    }
-}
+        
+//         for(int i =1;i<dp.length;i++){
+//             for(int j=1;j<dp[0].length;j++){
+//                 if(j>=wts[i-1]){
+//                     int rCap = j-wts[i-1];
+//                     if(dp[i-1][rCap]+vals[i-1]>dp[i-1][j]){
+//                         dp[i][j] = dp[i-1][rCap] + vals[i-1];
+//                     }else{
+//                         dp[i][j] = dp[i-1][j];
+//                     }
+//                 }else{
+//                     dp[i][j] = dp[i-1][j];
+//                 }
+//             }
+//         }
+        
+//         System.out.println(dp[n][capacity]);
+//         sc.close();
+//     }
+// }
+
