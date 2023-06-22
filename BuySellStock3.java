f9c6cadb41a37533addd9a0a263619f62305001b
Aryanmehta21 <aryanmehta310@gmail.com>
2023-06-23 02:00:35 +0530

    .

--- BuySellStock3.java
+++ BuySellStock3.java
@@ -12,26 +12,26 @@ public class BuySellStock3 {
         System.out.println(profit(arr, fee));
         sc.close();
     }
-
-    public static int profit(int[] arr, int fee){
-        int obsp = -arr[0];
-        int ossp = 0;
-        for(int i = 1;i<arr.length;i++){
-            int nbsp = 0;
-            int nssp = 0;
-            if(ossp - arr[i] > obsp){
-                nbsp = ossp - arr[i];
-            }else{
-                nbsp = obsp;
-            }
-            if(obsp + arr[i] - fee > ossp){
-                nssp = obsp + arr[i] - fee;
-            }else{
-                nssp = ossp;
-            }
-            obsp = nbsp;
-            ossp = nssp;
-        }
-        return ossp;
-    }
+    // Pepcoding:
+    // public static int profit(int[] arr, int fee){
+    //     int obsp = -arr[0];
+    //     int ossp = 0;
+    //     for(int i = 1;i<arr.length;i++){
+    //         int nbsp = 0;
+    //         int nssp = 0;
+    //         if(ossp - arr[i] > obsp){
+    //             nbsp = ossp - arr[i];
+    //         }else{
+    //             nbsp = obsp;
+    //         }
+    //         if(obsp + arr[i] - fee > ossp){
+    //             nssp = obsp + arr[i] - fee;
+    //         }else{
+    //             nssp = ossp;
+    //         }
+    //         obsp = nbsp;
+    //         ossp = nssp;
+    //     }
+    //     return ossp;
+    // }
 }
