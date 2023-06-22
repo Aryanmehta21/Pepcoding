6633465c11db7cfb2f3ced5b8d1ebf8afa9259e0
Aryanmehta21 <aryanmehta310@gmail.com>
2023-06-23 01:11:35 +0530

    Buy ans sell stocks 2

--- BuySellStock2.java
+++ BuySellStock2.java
@@ -20,5 +20,22 @@ public class BuySellStock2 {
         //     }
         // }
         // return profit;
+
+
+        // pepcoding
+
+        int buy = 0;
+        int sell = 0;
+        int profit = 0;
+        for(int i =1;i<prices.length;i++){
+            if(prices[i] >= prices[i-1]){
+                sell++;
+            }else{
+                profit += prices[sell] - prices[buy];
+                buy = sell = i; 
+            }
+        }
+        profit += prices[sell] - prices[buy];
+        return profit;
     }
 }
