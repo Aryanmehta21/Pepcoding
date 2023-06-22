33e4154d045478d41822d01ad8317f12e2935506
Aryanmehta21 <aryanmehta310@gmail.com>
2023-06-23 01:58:14 +0530

    .

--- BuySellStock3.java
+++ BuySellStock3.java
@@ -12,4 +12,18 @@ public class BuySellStock3 {
         System.out.println(profit(arr, fee));
         sc.close();
     }
+
+    public static int profit(int[] arr, int fee){
+        int obsp = -arr[0];
+        int ossp = 0;
+        for(int i = 1;i<arr.length;i++){
+            int nbsp = 0;
+            int nssp = 0;
+            if(ossp - arr[i] > obsp){
+                nbsp = ossp - arr[i];
+            }else{
+                nbsp = obsp;
+            }
+        }
+    }
 }
