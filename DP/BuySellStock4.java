57a07c5459704cd8519210c6fdc47f46ca31c84d
Aryanmehta21 <aryanmehta310@gmail.com>
2023-06-23 19:12:31 +0530

    .

--- DP/BuySellStock4.java
+++ DP/BuySellStock4.java
@@ -12,6 +12,29 @@ public class BuySellStock4 {
     }
 
     public static int profit(int[] prices){
-        
+        int mpist = 0;
+        int leastsf = prices[0];
+        int[] dpl = new int[prices.length];
+        for(int i = 1;i<prices.length;i++){
+            if(prices[i] < leastsf){
+                leastsf = prices[i];
+            }
+            mpist = prices[i] - leastsf;
+            if(mpist > dpl[i-1]){
+                dpl[i] = mpist;
+            }else{
+                dpl[i] = dpl[i-1];
+            }
+        }
+        int mpibt = 0;
+        int maxat = prices[prices.length-1];
+        int[] dpr = new int[prices.length];
+        for(int i = prices.length-2;i>=0;i--){
+            if(prices[i] > maxat){
+                maxat = prices[i];
+            }
+            mpibt = maxat - prices[i];
+            if(mpibt > )
+        }
     }
 }
