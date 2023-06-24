65140c4c50faafba311945364b94d0064b2cfe29
Aryanmehta21 <aryanmehta310@gmail.com>
2023-06-24 22:58:30 +0530

    .

--- TestClass.java
+++ TestClass.java
@@ -1,6 +1,5 @@
 // Question : https://www.hackerearth.com/practice/basic-programming/input-output/basics-of-input-output/practice-problems/algorithm/favourite-singer-a18e086a/
 import java.util.*;
-
 public class TestClass {
     public static void main(String args[] ) throws Exception {
         Scanner sc = new Scanner(System.in);
