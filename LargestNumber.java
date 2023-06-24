a03ba3d31da7fc3bfc854b2dee19e4ef68f9ab4b
Aryanmehta21 <aryanmehta310@gmail.com>
2023-06-25 00:03:02 +0530

    .

--- LargestNumber.java
+++ LargestNumber.java
@@ -1,17 +1,17 @@
 // Question : https://www.hackerearth.com/problem/algorithm/largest-number-10-ca319b09/?source=list_view
 import java.util.*;
-public class LargestNumber {
+
+class TestClass {
     public static void main(String args[] ) throws Exception {
         Scanner sc = new Scanner(System.in);
-        int no = sc.nextInt();
-        int k = sc.nextInt();
-        String str = Integer.toString(no);
+        long no = sc.nextLong();
+        long k = sc.nextLong();
+        String str = Long.toString(no);
         String ans = buildLargestNumber(str, k);
-        int a = Integer.parseInt(ans);
+        long a = Long.parseLong(ans);
         System.out.println(a);
-        sc.close();
     }
-    public static String buildLargestNumber(String number, int k) {
+    public static String buildLargestNumber(String number, long k) {
         int n = number.length();
         if (k >= n) {
             return "0";
