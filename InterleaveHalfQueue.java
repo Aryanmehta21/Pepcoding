050a7717e0725b29c9223ed4280a69f92b08f239
Aryanmehta21 <aryanmehta310@gmail.com>
2023-06-26 23:45:41 +0530

    .

--- InterleaveHalfQueue.java
+++ InterleaveHalfQueue.java
@@ -1,5 +1,8 @@
+import java.util.*;
 public class InterleaveHalfQueue {
     public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
         
     }
 }
