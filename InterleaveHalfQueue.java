244bc4248dce5467b07a1aed24f1c7a19387ada0
Aryanmehta21 <aryanmehta310@gmail.com>
2023-06-26 23:47:07 +0530

    .

--- InterleaveHalfQueue.java
+++ InterleaveHalfQueue.java
@@ -1,8 +1,15 @@
 import java.util.*;
+import java.util.LinkedList;
 public class InterleaveHalfQueue {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+        Queue<Integer> q = new LinkedList<>();
+        for(int i = 0;i<n;i++){
+            int x = sc.nextInt();
+            q.add(x);
+        }
+
         
     }
 }
