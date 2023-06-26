000257a3d99a91609623fbb3abf2cee7e1edb1f8
Aryanmehta21 <aryanmehta310@gmail.com>
2023-06-26 22:25:10 +0530

    .

--- FirstNonRepeating.java
+++ FirstNonRepeating.java
@@ -1,8 +1,12 @@
 import java.util.*;
+import java.util.LinkedList;
 public class FirstNonRepeating {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         String str = sc.nextLine();
+
+        int[] freq = new int[26];
+        Queue<Character> q = new LinkedList<>();
 
         
     }
