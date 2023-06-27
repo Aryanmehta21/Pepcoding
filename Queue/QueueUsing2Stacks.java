14b582577879e6dbde7a623980448708d25f6b6e
Aryanmehta21 <aryanmehta310@gmail.com>
2023-06-28 01:06:09 +0530

    .

--- Queue/QueueUsing2Stacks.java
+++ Queue/QueueUsing2Stacks.java
@@ -1,4 +1,16 @@
+import java.util.*;
 public class QueueUsing2Stacks {
+    class Queue{
+        Stack s1;
+        Stack s2;
+
+
+        void push(int data){
+            s1.push(data);
+        }
+
+        
+    }
     public static void main(String[] args){
         
     }
