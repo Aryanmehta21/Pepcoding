abc8b8785c86f174cd4dacfbe37154b950922ef7
Aryanmehta21 <aryanmehta310@gmail.com>
2023-06-28 01:16:22 +0530

    .

--- Queue/QueueUsing2Stacks.java
+++ Queue/QueueUsing2Stacks.java
@@ -1,15 +1,28 @@
 import java.util.*;
 public class QueueUsing2Stacks {
     class Queue{
-        Stack s1;
-        Stack s2;
+        static Stack<Integer> s1 = new Stack<>();
+        static Stack<Integer> s2 = new Stack<>();
 
 
-        void push(int data){
+        static void push(int data){
             s1.push(data);
         }
 
-        
+        static int pop(){
+            while(!s1.empty()){
+                s2.push(s1.pop());
+            }
+            int ans = s2.pop();
+            while(!s2.isEmpty()){
+                s1.push(s2.pop());
+            }
+            return ans;
+        }
+
+        static int peek(){
+
+        }
     }
     public static void main(String[] args){
         
