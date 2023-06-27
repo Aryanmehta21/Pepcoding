dcbc68eb772d5e236725a6aed4f5b2d0add0ff93
Aryanmehta21 <aryanmehta310@gmail.com>
2023-06-28 00:11:31 +0530

    .

--- Queue/QueueUsingLinkedList.java
+++ Queue/QueueUsingLinkedList.java
@@ -1,5 +1,21 @@
 import java.util.*;
 public class QueueUsingLinkedList {
+    class Node{
+        int data;
+        Node next;
+        public Node(int data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    class Queue{
+        public static Node head, tail = null;
+        
+        public static boolean isEmpty(){
+
+        }
+    }
     public static void main(String[] args){
 
     }
