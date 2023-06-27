eb11bcd59adb1ef5477c445a5a511272e18ad3a5
Aryanmehta21 <aryanmehta310@gmail.com>
2023-06-28 00:45:54 +0530

    .

--- Queue/QueueUsingLinkedList.java
+++ Queue/QueueUsingLinkedList.java
@@ -42,6 +42,9 @@ public class QueueUsingLinkedList {
     }
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        
+        int n = sc.nextInt();
+        for(int i = 0;i<n;i++){
+            
+        }
     }
 }
