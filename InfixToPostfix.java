9d6292b636fe4ce3bb54d3597ddef3c73e06b2e2
Aryanmehta21 <aryanmehta310@gmail.com>
2023-06-29 00:33:19 +0530

    .

--- InfixToPostfix.java
+++ InfixToPostfix.java
@@ -2,6 +2,8 @@ import java.util.*;
 public class InfixToPostfix{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        
+        String str = sc.nextLine();
+        System.out.println(convert(str));
+        sc.close();
     }
 }
