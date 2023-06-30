b3e6fdd5f060c9148629a8eb3e5fb962dffc2ae9
Aryanmehta21 <aryanmehta310@gmail.com>
2023-07-01 02:08:34 +0530

    .

--- Queue/ConnectNropes.java
+++ Queue/ConnectNropes.java
@@ -1,7 +1,7 @@
 // Question: Connect N ropes with minimum cost given array of length of ropes and the cost can be calculated by adding two ropes of length;
 // I/p : [2,5,4,8,6,9]
 // o/p : 85
-// Add two smallest ropes one by one.
+// approach : Add two smallest ropes one by one to the priority queue(min heap) which is by default in java.
 
 import java.util.*;
 public class ConnectNropes {
