411705d5c7e624e1ffe1b0fae688618de3865bbb
Aryanmehta21 <aryanmehta310@gmail.com>
2023-07-02 23:01:53 +0530

    .

--- DP/ShortestStringSubsequence.java
+++ DP/ShortestStringSubsequence.java
@@ -4,9 +4,14 @@ public class ShortestStringSubsequence {
         int[][] dp = new int[text1.length() + 1][text2.length() + 1];
         for(int i = 1;i<dp.length;i++){
             for(int j = 1;j<dp[0].length;j++){
-                
+                if(text1.charAt(i-1) == text2.charAt(j-1)){
+                    dp[i][j] = dp[i-1][j-1] + 1;
+                }else{
+                    dp[i][j] = Math.max(dp[i][j-1], dp[i-1][j]);
+                }
             }
         }
+        return dp[text1.length()][text2.length()];
     }
 
 
@@ -14,6 +19,8 @@ public class ShortestStringSubsequence {
         int lcs = LCS(text1, text2);
         return text1.length() + text2.length() - lcs;
     }
+
+
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         String str1 = sc.nextLine();
