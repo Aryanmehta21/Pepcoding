d724b2330ef7a4172ef5e629fdd17ca2285e557f
Aryanmehta21 <aryanmehta310@gmail.com>
2023-07-02 22:30:54 +0530

    .

--- DP/LongestCommonSubsequence.java
+++ DP/LongestCommonSubsequence.java
@@ -2,9 +2,11 @@ import java.util.*;
 public class LongestCommonSubsequence {
 
     public static int LCS(String text1, String text2){
-        
+
     }
     public static void main(String[] args){
-
+        Scanner sc = new Scanner(System.in);
+        String str1 = sc.nextLine();
+        String str2 = sc.nextLine();
     }
 }
