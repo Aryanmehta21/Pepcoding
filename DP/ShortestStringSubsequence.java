ba78e8473d5ebd40c50793739a0244b0c9b9e728
Aryanmehta21 <aryanmehta310@gmail.com>
2023-07-02 22:58:50 +0530

    .

--- DP/ShortestStringSubsequence.java
+++ DP/ShortestStringSubsequence.java
@@ -1,5 +1,15 @@
 import java.util.*;
 public class ShortestStringSubsequence {
+    public static int LCS(String text1, String text2){
+        int[][] dp = new int[text1.length() + 1][text2.length() + 1];
+        for(int i = 1;i<dp.length;i++){
+            for(int j = 1;j<dp[0].length;j++){
+                
+            }
+        }
+    }
+
+
     public static int lengthOfString(String text1, String text2){
         int lcs = LCS(text1, text2);
         return text1.length() + text2.length() - lcs;
@@ -11,5 +21,6 @@ public class ShortestStringSubsequence {
 
 
         System.out.println(lengthOfString(str1, str2));
+        sc.close();
     }
 }
