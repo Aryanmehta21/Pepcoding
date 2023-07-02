02de96cb62e4167cc74533ef0607a05d0b4d29d0
Aryanmehta21 <aryanmehta310@gmail.com>
2023-07-02 22:36:34 +0530

    .

--- DP/LongestCommonSubsequence.java
+++ DP/LongestCommonSubsequence.java
@@ -5,9 +5,14 @@ public class LongestCommonSubsequence {
         int[][] dp = new int[text1.length() + 1][text2.length()+1];
         for(int i = 0;i<dp.length;i++){
             for(int j = 0;j<dp[0].length;j++){
-                
+                if(text1.charAt(i-1) == text2.charAt(j-1)){
+                    dp[i][j] = dp[i-1][j-1] + 1;
+                }else{
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+                }
             }
         }
+        
     }
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
