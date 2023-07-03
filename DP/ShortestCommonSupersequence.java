5af7624f26416f73f4a6535ac778b865e4e5d959
Aryanmehta21 <aryanmehta310@gmail.com>
2023-07-03 18:48:48 +0530

    .

--- DP/ShortestCommonSupersequence.java
+++ DP/ShortestCommonSupersequence.java
@@ -1,5 +1,14 @@
 import java.util.*;
 public class ShortestCommonSupersequence {
+
+    public static String LCS(String text1, String text2){
+
+    }
+
+    public static String shortestCommonSupersequence(String text1, String text2){
+
+    }
+
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         String str1 = sc.nextLine();
