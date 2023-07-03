66723630d196ebbe7263ccfa9c4b26fc18463483
Aryanmehta21 <aryanmehta310@gmail.com>
2023-07-03 18:50:22 +0530

    .

--- DP/ShortestCommonSupersequence.java
+++ DP/ShortestCommonSupersequence.java
@@ -5,8 +5,30 @@ public class ShortestCommonSupersequence {
 
     }
 
-    public static String shortestCommonSupersequence(String text1, String text2){
+    public static String shortestCommonSupersequence(String str1, String str2){
+        String lcs = LCS(str1, str2);
+        int p1 = 0;
+        int p2 = 0;
+        String ans = "";
+        for(int i = 0;i<lcs.length();i++){
+            char ch = lcs.charAt(i);
 
+            while(str1.charAt(p1) != ch){
+                ans += str1.charAt(p1);
+                p1++;
+            }
+
+            while(str2.charAt(p2) != ch){
+                ans += str2.charAt(p2);
+                p2++;
+            }
+            ans+=ch;
+            p1++;
+            p2++;
+
+        }
+        ans+=str1.substring(p1) + str2.substring(p2);
+        return ans;
     }
 
     public static void main(String[] args){
