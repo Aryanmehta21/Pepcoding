ff01d1cb51ea9318621d064443fa7b407774a4a3
Aryanmehta21 <aryanmehta310@gmail.com>
2023-07-03 23:09:22 +0530

    .

--- Binary Search/SearchInInfiniteSortedArray.java
+++ Binary Search/SearchInInfiniteSortedArray.java
@@ -10,6 +10,15 @@ public class SearchInInfiniteSortedArray{
 
         int key = sc.nextInt();
 
-        
+        int low = 0;
+        int high = 1;
+
+        while(arr[high] < key){
+            low = high;
+            high = 2*high;
+        }
+
+        System.out.println(binarySearch(arr, key, low, high));
+
     }
 }
