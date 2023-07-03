869f165a6b0431c13ade1821227ac8a4cbdc7320
Aryanmehta21 <aryanmehta310@gmail.com>
2023-07-04 00:10:02 +0530

    .

--- Binary Search/SearchInRotatedSortedArray.java
+++ Binary Search/SearchInRotatedSortedArray.java
@@ -1,6 +1,11 @@
 import java.util.*;
 public class SearchInRotatedSortedArray {
     public static void main(String[] args){
-
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr =new int[n];
+        for(int i =0;i<arr.length;i++){
+            
+        }
     }
 }
