e8e4625645a7c13d52ee032d2499745c0a553cba
Aryanmehta21 <aryanmehta310@gmail.com>
2023-07-04 00:18:37 +0530

    .

--- Binary Search/SearchInRotatedSortedArray.java
+++ Binary Search/SearchInRotatedSortedArray.java
@@ -1,10 +1,11 @@
 import java.util.*;
+
 public class SearchInRotatedSortedArray {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int[] arr =new int[n];
-        for(int i =0;i<arr.length;i++){
+        int[] arr = new int[n];
+        for (int i = 0; i < arr.length; i++) {
             arr[i] = sc.nextInt();
         }
         int key = sc.nextInt();
@@ -12,14 +13,19 @@ public class SearchInRotatedSortedArray {
         sc.close();
     }
 
-
-    public static int search(int[] arr, int target){
+    public static int search(int[] arr, int target) {
         int low = 0;
         int high = arr.length - 1;
 
-        while(low<=high){
+        while (low <= high) {
             int mid = low + (high - low) / 2;
-            if(arr[mid] == target) return mid;
+            if (arr[mid] == target)
+                return mid;
+            if (arr[low] <= arr[mid]) {
+                if (target >= arr[low] && target < mid) {
+                    
+                }
+            }
         }
     }
 }
