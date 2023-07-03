3504fa1bfbd0c85004e052cf0fb36807836dabbb
Aryanmehta21 <aryanmehta310@gmail.com>
2023-07-03 18:47:29 +0530

    .

--- DP/ShortestCommonSupersequence.java
+++ DP/ShortestCommonSupersequence.java
@@ -2,6 +2,10 @@ import java.util.*;
 public class ShortestCommonSupersequence {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        
+        String str1 = sc.nextLine();
+        String str2 = sc.nextLine();
+
+        System.out.println(shortestCommonSupersequence(str1, str2));
+        sc.close();
     }
 }
