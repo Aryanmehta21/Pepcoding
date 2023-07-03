d64a58c66dde79e44d30938f567509b460522186
Aryanmehta21 <aryanmehta310@gmail.com>
2023-07-03 23:25:58 +0530

    .

--- Binary Search/SearchInInfiniteSortedArray.java
+++ Binary Search/SearchInInfiniteSortedArray.java
@@ -23,6 +23,18 @@ public class SearchInInfiniteSortedArray{
     }
     public static int binarySearch(int[] arr, int key, int low, int high){
 
-        
+        while(low<=high){
+            int mid = low + (high - low)/2;
+
+            if(arr[mid] == key){
+                return mid;
+            }else if(arr[mid] < key){
+                high = mid-1;
+            }else{
+                low = mid +1;
+            }
+        }
+
+        return -1;
     }
 }
