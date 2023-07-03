a93dee44fd6c8ae721e0fa0ad317ce130bce3066
Aryanmehta21 <aryanmehta310@gmail.com>
2023-07-03 18:50:47 +0530

    .

--- DP/ShortestCommonSupersequence.java
+++ DP/ShortestCommonSupersequence.java
@@ -2,7 +2,23 @@ import java.util.*;
 public class ShortestCommonSupersequence {
 
     public static String LCS(String text1, String text2){
-
+        String[][] dp = new String[text1.length() + 1][text2.length() + 1];
+        for(int i = 0;i<=text1.length();i++){
+            for(int j = 0;j<=text2.length();j++){
+                if(i==0||j==0){
+                    dp[i][j] = "";
+                }else if(text1.charAt(i-1) == text2.charAt(j-1)){
+                    dp[i][j] = dp[i-1][j-1] + text1.charAt(i-1);
+                }else{
+                    if(dp[i-1][j].length() >= dp[i][j-1].length()){
+                        dp[i][j] = dp[i-1][j];
+                    }else{
+                        dp[i][j] = dp[i][j-1];
+                    }
+                }
+            }
+        }
+        return dp[text1.length()][text2.length()];
     }
 
     public static String shortestCommonSupersequence(String str1, String str2){
