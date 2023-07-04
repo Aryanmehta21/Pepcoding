59420da6e92365053ae41d71da410d37244ac1a3
Aryanmehta21 <aryanmehta310@gmail.com>
2023-07-04 13:49:30 +0530

    .

--- Binary Search/FindPeakElementinArray.java
+++ Binary Search/FindPeakElementinArray.java
@@ -1,6 +1,13 @@
 import java.util.*;
 public class FindPeakElementinArray {
     public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i =0;i<arr.length;i++){
+            arr[i] = sc.nextInt();
+        }
 
+        System.out.println(findPeak(arr));
     }
 }
