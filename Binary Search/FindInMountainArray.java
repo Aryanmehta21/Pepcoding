037638a918eebe62a846ab97e43ab196c8e6ea80
Aryanmehta21 <aryanmehta310@gmail.com>
2023-07-05 01:17:38 +0530

    .

--- Binary Search/FindInMountainArray.java
+++ Binary Search/FindInMountainArray.java
@@ -1,6 +1,13 @@
 import java.util.*;
 public class FindInMountainArray {
     public static void main(String[] args){
-
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0 ;i<arr.length;i++){
+            arr[i] = sc.nextInt();
+        }
+        int target = sc.nextInt();
+        System.out.println(search(target, arr));
     }
 }
