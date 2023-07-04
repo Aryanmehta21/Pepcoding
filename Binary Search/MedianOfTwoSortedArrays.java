1b2cce096abd41e635ec101c1a072939ba8bd849
Aryanmehta21 <aryanmehta310@gmail.com>
2023-07-05 00:06:08 +0530

    .

--- Binary Search/MedianOfTwoSortedArrays.java
+++ Binary Search/MedianOfTwoSortedArrays.java
@@ -33,14 +33,18 @@ public class MedianOfTwoSortedArrays {
             int i2 = (a.length + b.length + 1)/2 - i1;
 
 
-            int min1 = (i1 == n) ? Integer.MAX_VALUE : a[i1];
+            int min1 = (i1 == a.length) ? Integer.MAX_VALUE : a[i1];
             int max1 = (i1 == 0) ? Integer.MIN_VALUE : a[i1 -1];
 
-            int min2 = (i2 == m) ? Integer.MAX_VALUE : b[i2];
+            int min2 = (i2 == b.length) ? Integer.MAX_VALUE : b[i2];
             int max2 = (i2 == 0) ? Integer.MIN_VALUE : b[i2 - 1];
 
+            if((max1<=min2) && (max2 <= min1)){
+                if(a.length + b.length) % 2 == 0){
+                    return((double) (Math.max(max1, max2) + Math.min(min1, min2))/2);
+                }
+            }
 
-            
         }
     }
 }
