1bab8b9c15b223e527826e6b94465171a461485b
Aryanmehta21 <aryanmehta310@gmail.com>
2023-07-04 23:56:05 +0530

    .

--- Binary Search/MedianOfTwoSortedArrays.java
+++ Binary Search/MedianOfTwoSortedArrays.java
@@ -1,5 +1,20 @@
+import java.util.*;
 public class MedianOfTwoSortedArrays {
     public static void main(String[] args){
-        
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0;i<arr.length;i++){
+            arr[i] = sc.nextInt();
+        }
+
+        int m = sc.nextInt();
+        int[] brr = new int[m];
+        for(int i =0;i<m;i++){
+            brr[i] = sc.nextInt();
+        }
+
+        System.out.println(median(arr, brr));
+        sc.close();
     }
 }
