f3117493eaec6360522aeaffb214f008d7d61fc3
Aryanmehta21 <aryanmehta310@gmail.com>
2023-07-06 01:52:46 +0530

    .

--- Sorting/CountingSort.java
+++ Sorting/CountingSort.java
@@ -1,6 +1,19 @@
 import java.util.Scanner;
 
 public class CountingSort {
+
+    public static void countingSort(int[] arr){
+        int largest = Integer.MIN_VALUE;
+        for(int i = 0;i<arr.length;i++){
+            largest = Math.max(largest, arr[i]);
+        }
+
+        int[] count = new int[largest + 1];
+        for(int i = 0;i<arr.length;i++){
+            count[arr[i]]++;
+        }
+    }
+
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
@@ -8,7 +21,6 @@ public class CountingSort {
         for(int i = 0;i<n;i++){
             arr[i] = sc.nextInt();
         }
-
         countingSort(arr);
         for(int i = 0;i<arr.length;i++){
             System.out.print(arr[i] + " ");
