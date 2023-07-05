bf17e5153e9f732f117ebfc1ff116e305089299c
Aryanmehta21 <aryanmehta310@gmail.com>
2023-07-06 01:42:37 +0530

    .

--- Sorting/InsertionSort.java
+++ Sorting/InsertionSort.java
@@ -1,6 +1,9 @@
 import java.util.Scanner;
 
 public class InsertionSort {
+    public static void insertionSort(int[] arr){
+        
+    }
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
@@ -9,7 +12,7 @@ public class InsertionSort {
             arr[i] = sc.nextInt();
         }
 
-        selectionSort(arr);
+        insertionSort(arr);
         for(int i = 0;i<arr.length;i++){
             System.out.print(arr[i] + " ");
         }
