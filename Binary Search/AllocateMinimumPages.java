8bf91dd3e1a826af794ee8ac9c8cd83fef433793
Aryanmehta21 <aryanmehta310@gmail.com>
2023-07-05 12:22:50 +0530

    .

--- Binary Search/AllocateMinimumPages.java
+++ Binary Search/AllocateMinimumPages.java
@@ -23,6 +23,7 @@ public class AllocateMinimumPages {
         }
         return sum;
     }
+    
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
