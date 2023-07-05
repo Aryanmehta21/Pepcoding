c8d8a0609d1f2b0accd812f6cceb009baf35fd02
Aryanmehta21 <aryanmehta310@gmail.com>
2023-07-06 01:31:52 +0530

    .

--- Sorting/SelectionSort.java
+++ Sorting/SelectionSort.java
@@ -2,8 +2,27 @@ import java.util.Scanner;
 
 public class SelectionSort {
     public static void selectionSort(int[] arr){
+        for(int i = 0;i<arr.length;i++){
+            int last = arr.length-i-1;
+            int maxIndex = getMaxIndex(arr, 0, last);
+            swap(arr, maxIndex, last);
+        }
+    }
+
+    public static int getMaxIndex(int[] arr, int start, int end){
+        int max = start;
+        for(int i = start;i<=end;i++){
+            if(arr[i] > arr[max]){
+                max = arr[i];
+            }
+        }
+        return max;
+    }
+
+    public static void swap(int[] arr, int first, int second){
         
     }
+
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
