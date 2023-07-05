98b097c10965a837f8bf2bae8a59e7d3ac4681dd
Aryanmehta21 <aryanmehta310@gmail.com>
2023-07-06 01:44:16 +0530

    .

--- Sorting/InsertionSort.java
+++ Sorting/InsertionSort.java
@@ -2,7 +2,17 @@ import java.util.Scanner;
 
 public class InsertionSort {
     public static void insertionSort(int[] arr){
-        
+        for(int i = 0;i<arr.length;i++){
+            for(int j = i+1;j>0;j--){
+                if(arr[j] < arr[j-1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j-1];
+                    arr[j-1] = temp;
+                }else{
+                    break;
+                }
+            }
+        }
     }
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
