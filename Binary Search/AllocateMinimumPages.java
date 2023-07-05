689d4f16ca0c566c1e8ae0996459b7840968185b
Aryanmehta21 <aryanmehta310@gmail.com>
2023-07-05 12:24:06 +0530

    .

--- Binary Search/AllocateMinimumPages.java
+++ Binary Search/AllocateMinimumPages.java
@@ -4,6 +4,16 @@ public class AllocateMinimumPages {
         int min = maxOf(books);
         int max = sumOf(books);
         int res = 0;
+        while(min<=max){
+            int mid = min + (max - min)/2;
+            if(isFeasible(books, k, mid)){
+                res = mid;
+                max = mid-1;
+            }else{
+                min = mid + 1;
+            }
+        }
+        return res;
     }
     
     public static int maxOf(int[] books){
@@ -23,7 +33,7 @@ public class AllocateMinimumPages {
         }
         return sum;
     }
-    
+
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
