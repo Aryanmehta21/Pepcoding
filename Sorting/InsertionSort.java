999b9b3ab3dc737a3334c023780de8c75969194f
Aryanmehta21 <aryanmehta310@gmail.com>
2023-07-06 01:46:43 +0530

    .

--- Sorting/InsertionSort.java
+++ Sorting/InsertionSort.java
@@ -3,15 +3,14 @@ import java.util.Scanner;
 public class InsertionSort {
     public static void insertionSort(int[] arr){
         for(int i = 0;i<arr.length;i++){
-            for(int j = i+1;j>0;j--){
-                if(arr[j] < arr[j-1]){
-                    int temp = arr[j];
-                    arr[j] = arr[j-1];
-                    arr[j-1] = temp;
-                }else{
-                    break;
-                }
+            int curr = arr[i];
+            int prev = i-1;
+            while(prev >= 0&&arr[prev]>curr){
+                arr[prev+1] = arr[prev];
+                prev--;
             }
+            arr[prev+1] = curr;
+
         }
     }
     public static void main(String[] args){
