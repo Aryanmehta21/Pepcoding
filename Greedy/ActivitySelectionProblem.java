7420a11543af6b5395fbe229d77f037e2c421cc9
Aryanmehta21 <aryanmehta310@gmail.com>
2023-07-07 21:27:15 +0530

    .

--- Greedy/ActivitySelectionProblem.java
+++ Greedy/ActivitySelectionProblem.java
@@ -1,6 +1,15 @@
 import java.util.*;
 public class ActivitySelectionProblem{
     public static void main(String[] args){
-        
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] start = new int[n];
+        int[] end= new int[n];
+        for(int i = 0;i<start.length;i++){
+            start[i] = sc.nextInt();
+        }
+        for(int i = 0;i<end.length;i++){
+            end[i] = sc.nextInt();
+        }
     }
 }
