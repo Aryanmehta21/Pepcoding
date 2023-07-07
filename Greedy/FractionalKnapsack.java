8a49da7466b861961a90e35577d0c3527cff9f6b
Aryanmehta21 <aryanmehta310@gmail.com>
2023-07-08 00:16:35 +0530

    .

--- Greedy/FractionalKnapsack.java
+++ Greedy/FractionalKnapsack.java
@@ -15,7 +15,8 @@ public class FractionalKnapsack {
 
         double[][] ratio = new double[val.length][2];
         for(int i = 0;i<ratio.length;i++){
-            
+            ratio[i][0] = i;
+            ratio[i][1] = val[i] / (double)weights[i];
         }
     }
 }
