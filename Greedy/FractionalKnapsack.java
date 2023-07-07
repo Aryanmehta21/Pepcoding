8fc1c720634d609ebc3568fd2326b59cd31c4bba
Aryanmehta21 <aryanmehta310@gmail.com>
2023-07-08 00:14:12 +0530

    .

---
+++ Greedy/FractionalKnapsack.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class FractionalKnapsack {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] val = new int[n];
+        int[] weights = new int[n];
+        
+    }
+}
