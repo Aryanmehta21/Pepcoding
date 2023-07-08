0f1b0d473a668bcc01e1b1e480fc95bdc4e3aa72
Aryanmehta21 <aryanmehta310@gmail.com>
2023-07-08 15:51:48 +0530

    .

---
+++ Greedy/MinimumCoins.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class MinimumCoins {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int[] coins = {1, 2, 5, 10, 20, 50, 100, 500, 2000};
+
+        
+    }
+}
