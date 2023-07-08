e64d5c0eadc2b761743273920225931f95a0cbfe
Aryanmehta21 <aryanmehta310@gmail.com>
2023-07-08 15:08:20 +0530

    .

--- Greedy/MaximumLengthOfChain.java
+++ Greedy/MaximumLengthOfChain.java
@@ -6,8 +6,22 @@ public class MaximumLengthOfChain {
         int[][] pairs = new int[n][2];
         for(int i = 0;i<pairs.length;i++){
             for(int j = 0;j<pairs[0].length;j++){
-                
+                pairs[i][j] = sc.nextInt();
             }
         }
+
+        Arrays.sort(pairs, (a,b) -> Integer.compare(a[1], b[1]));    //Sorting on the basis of first column. Lambda Function in java
+
+        int chainLen = 0;
+        int chainEnd = pairs[0][1];
+
+        for(int i =1;i<pairs.length;i++){
+            if(pairs[i][0] > chainEnd){
+                chainLen++;
+                chainEnd = pairs[i][1];
+            }
+        }
+
+        System.out.println("Max Length of the chain: " + chainLen);
     } 
 }
