92096187b7c20408151daa640a51c67a2b3119a2
Aryanmehta21 <aryanmehta310@gmail.com>
2023-07-08 15:04:07 +0530

    .

--- Greedy/MaximumLengthOfChain.java
+++ Greedy/MaximumLengthOfChain.java
@@ -2,6 +2,12 @@ import java.util.*;
 public class MaximumLengthOfChain {
     public static void main(String[]args){
         Scanner sc = new Scanner(System.in);
-        
+        int n = sc.nextInt();
+        int[][] pairs = new int[n][2];
+        for(int i = 0;i<pairs.length;i++){
+            for(int j = 0;j<pairs[0].length;j++){
+                
+            }
+        }
     } 
 }
