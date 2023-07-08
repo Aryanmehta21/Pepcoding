77e621fd972d7af328ca45a40080fc17a2ef4059
Aryanmehta21 <aryanmehta310@gmail.com>
2023-07-08 16:53:28 +0530

    .

--- Greedy/JobSequencingProblem.java
+++ Greedy/JobSequencingProblem.java
@@ -6,8 +6,19 @@ public class JobSequencingProblem {
         int[][] jobs = new int[n][2];
         for(int i = 0;i<jobs.length;i++){
             for(int j = 0;j<jobs[0].length;j++){
-                
+                jobs[i][j] = sc.nextInt();
             }
         }
+
+        int[][] jobsInfo = new int[n][3];
+        for(int i = 0;i<jobsInfo.length;i++){
+            jobsInfo[i][0] = i;
+            jobsInfo[i][1] = jobs[i][0];
+            jobsInfo[i][2] = jobs[i][1];
+        }
+
+        Arrays.sort(jobsInfo, (a,b) -> Integer.compare(b[2], a[2]));
+
+        
     }
 }
