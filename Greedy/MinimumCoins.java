2db51cf2c546f89af7b86f28898799e613fe12f1
Aryanmehta21 <aryanmehta310@gmail.com>
2023-07-08 15:57:45 +0530

    .

--- Greedy/MinimumCoins.java
+++ Greedy/MinimumCoins.java
@@ -2,7 +2,12 @@ import java.util.*;
 public class MinimumCoins {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        int[] coins = {1, 2, 5, 10, 20, 50, 100, 500, 2000};
+        Integer[] coins = {1, 2, 5, 10, 20, 50, 100, 500, 2000};
+        
+        Arrays.sort(coins, Comparator.reverseOrder());
+        
+        int countOdCoins = 0;
+        int amount = sc.nextInt();
 
         
     }
