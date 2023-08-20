cc74e1d5f40b302ef438132f6555cbe034397faa
Aryanmehta21 <aryanmehta310@gmail.com>
2023-08-20 18:28:35 +0530

    Update IntegerToRoman.java

--- IntegerToRoman.java
+++ IntegerToRoman.java
@@ -4,8 +4,8 @@ public class IntegerToRoman {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
-        int[] IntegerValue = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
-        String[] Roman = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        int[] IntegerValue = {5000, 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        String[] Roman = {"H", "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
         String ans = "";
         for(int i = 0;i<IntegerValue.length;i++){
             while(n >= IntegerValue[i]){
